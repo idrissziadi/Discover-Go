437b0d0a00e7416ef88a4148fe85794b87b5c402
Idrissziadi <143364738+idrissziadi@users.noreply.github.com>
2024-09-01 16:46:15 +0200

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,19 +1,24 @@
-// middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-
+  // Extraction du token depuis l'en-tête Authorization
   const token = req.headers['authorization']?.split(' ')[1];
-  if (token == null) return res.status(401).json({ message: 'Token required' });
+  
+  // Vérification de la présence du token
+  if (!token) return res.status(401).json({ message: 'Token required' });
 
+  // Vérification de la validité du token
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: 'Invalid token' });
+    
+    // Ajout des informations de l'utilisateur décodé dans la requête
     req.user = user;
     next();
   });
 };
 
 const authorizeAdmin = (req, res, next) => {
+  // Vérification du rôle de l'utilisateur
   if (req.user.role !== 'admin') {
     return res.status(403).json({ message: 'Access denied' });
   }
