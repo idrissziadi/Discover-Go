2a82d19f95191ca7121533b453a782aa500bd588
Idrissziadi <143364738+idrissziadi@users.noreply.github.com>
2024-09-01 16:43:18 +0200

    Update siteController.js

--- backend/controllers/siteController.js
+++ backend/controllers/siteController.js
@@ -1,8 +1,9 @@
 // controllers/siteController.js
 const  Site = require('../models/site');
 const Image = require('../models/image'); 
-
-
+const Review = require('../models/review');
+const User = require('../models/user');
+const Event = require('../models/event');
 exports.createSite = async (req, res) => {
   try {
     const site = await Site.create(req.body);
@@ -14,7 +15,9 @@ exports.createSite = async (req, res) => {
 
 exports.getSites = async (req, res) => {
   try {
+    console.log("hahahha1");
     const sites = await Site.findAll();
+    console.log("hahahha2");
     res.status(200).json(sites);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -86,3 +89,39 @@ exports.getSitesBySubcategoryId = async (req, res) => {
       res.status(500).json({ message: error.message });
     }
   };
+
+  // Method to get all sites with their reviews and associated user names
+exports.getSitesWithReviews = async (req, res) => {
+  try {
+    const sites = await Site.findAll({
+      include: [
+        {
+          model: Review,
+          attributes: ['id', 'rating', 'comment', 'createdAt'], // Include desired Review attributes
+          include: [
+            {
+              model: User,
+              attributes: ['username'], // Include only the username of the user
+            }
+          ]
+        },
+        {
+          model: Image,
+          attributes: ['id', 'imageUrl'],
+        },
+        {
+          model : Event,
+          attributes : ['id' , 'title' , 'description' , 'event_date' ], 
+        }
+      ]
+    });
+
+    if (sites.length > 0) {
+      res.status(200).json(sites);
+    } else {
+      res.status(404).json({ message: 'No sites found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
