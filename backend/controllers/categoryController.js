77f4108cd8561226fa1e0b2e736412e933478acc
Idrissziadi <143364738+idrissziadi@users.noreply.github.com>
2024-09-01 16:42:26 +0200

    Update categoryController.js

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,4 +1,4 @@
-// controllers/categoryController.jss
+// controllers/categoryController.js
 const  Category = require('../models/category');
 const Subcategory = require('../models/subcategory');
 
